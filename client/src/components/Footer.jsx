b45b9826ecf629139b4302d696677ef50cdd621c
MisterMickey <singodiyapeechuparashubham@gmail.com>
2023-07-29 16:49:05 +0530

    change logout button and modify footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,3 +1,13 @@
+import { BsGithub } from "react-icons/bs";
+import "../styles/Footer.css"
+
 export default function Footer() {
-  return <div></div>
+  return (
+    <div className="footer">
+      <a className="ghIcon" href="https://github.com/PekaVengers/GitCollabX">
+        <BsGithub className="githubIcon" />
+      </a>
+      <span className="copyright">©2023 | ALL RIGHTS RESERVED</span>
+    </div>
+  );
 }
