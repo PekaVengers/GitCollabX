6697bdcaeaf28e907e1ea7ec173955fc0396ac99
Kailash Choudhary <kailashjat604@gmail.com>
2023-07-29 10:51:40 +0530

    refactor: add data in json file

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -2,39 +2,9 @@ import { DiGithubBadge } from "react-icons/di";
 import { Link } from "react-router-dom";
 import ProgressBar from "@ramonak/react-progress-bar";
 import "../styles/Projects.css";
+import projects from "../db/projects.json"
 
 export default function Projects() {
-
-  const projects = [
-    {
-      id: "1",
-      name: "Project 1",
-      githubRepo: "https://github.com/kailashchoudhary11/ideator",
-      techStack: "HTML, CSS, JavaScript",
-      percentageCompleted: "50",
-    },
-    {
-      id: "2",
-      name: "Project 1",
-      githubRepo: "https://github.com/kailashchoudhary11/ideator",
-      techStack: "HTML, CSS, JavaScript",
-      percentageCompleted: "50",
-    },
-    {
-      id: "3",
-      name: "Project 1",
-      githubRepo: "https://github.com/kailashchoudhary11/ideator",
-      techStack: "HTML, CSS, JavaScript",
-      percentageCompleted: "50",
-    },
-    {
-      id: "4",
-      name: "Project 1",
-      githubRepo: "https://github.com/kailashchoudhary11/ideator",
-      techStack: "HTML, CSS, JavaScript",
-      percentageCompleted: "50",
-    },
-  ]
 
   return (
     <div className="projects-page-container">
