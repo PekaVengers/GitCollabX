e86f04e69eecb3ceabc0caba69ad8f55111098ea
MisterMickey <singodiyapeechuparashubham@gmail.com>
2023-07-29 15:45:43 +0530

    logout button styled

--- client/src/components/LogoutButton.jsx
+++ client/src/components/LogoutButton.jsx
@@ -1,9 +1,10 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import "../styles/LogoutButton.css"
 
 export default function LogoutButton() {
   const {logout} = useAuth0();
   return (
-    <button onClick={() => logout()}>
+    <button onClick={() => logout()} className="logoutBtn">
       Log Out
     </button>
   )
